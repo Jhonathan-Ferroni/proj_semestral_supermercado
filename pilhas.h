22aa11191cb004cc2c10c30a2f5a18dee83ce002
Jhonathan Ferroni <ferroni.jhonathan2403@gmail.com>
2025-01-06 19:59:37 -0300

    Update pilhas.h

--- pilhas.h
+++ pilhas.h
@@ -1,10 +1,46 @@
-// utils.h
 #ifndef PILHAS_H
 #define PILHAS_H
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXTAM 5
+#define NUM_GONDOLAS 10
+
+typedef struct
+{
+    char NOMEPROD[100 + 1];
+    char DATAVENC[10 + 1];
+    char DATAVAL[10 + 1];
+    int preco;
+} REGISTRO;
+
+typedef struct
+{
+    REGISTRO elementos[MAXTAM];
+    int topo;
+} PILHA;
+
+/*struct do carrinho*/
+
+typedef struct aux
+{
+    REGISTRO reg;
+    struct aux* PROX;
+} ELEMENTO;
+
+typedef ELEMENTO* PONT;
+
+typedef struct
+{
+    PONT topo;
+} CARRINHO;
+
 // Declaração das funções
-int vertamanho (PILHA * p);
-void exibirPilha(PILHA * p);
+int vertamanho(PILHA* p);
+void exibirPilha(PILHA* p);
 bool estaVazia(PILHA* p);
 bool estaCheia(PILHA* p);
 bool inserirElemPilha(PILHA* p, REGISTRO reg);
@@ -16,6 +52,6 @@ void inicializarPilha(PILHA* p);
 // Funções utilitárias
 void exibirGondolas(PILHA gondolas[], int numGondolas);
 void exibirCarrinho(CARRINHO* carrinho);
-void menu(PILHA gondolas[], int numGondolas, CARRINHO* carrinho, char usuario);
+void menupilhas(PILHA gondolas[], int numGondolas, CARRINHO* carrinho, char usuario); // Alterado de menu() para menupilhas()
 
 #endif
