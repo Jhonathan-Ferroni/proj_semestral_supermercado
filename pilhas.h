a3cef652a9cc7d435e4761287a80188024d1d08d
Jhonathan Ferroni <ferroni.jhonathan2403@gmail.com>
2025-01-11 13:47:22 -0300

    Update pilhas.h

--- pilhas.h
+++ pilhas.h
@@ -5,16 +5,24 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
 #define MAXTAM 5
 #define NUM_GONDOLAS 10
 
+
+typedef struct
+{
+    char nome[50];
+    char pront[14];
+    char user[2];
+} reg;
+
 typedef struct
 {
     char NOMEPROD[100 + 1];
-    char DATAVENC[10 + 1];
-    char DATAVAL[10 + 1];
-    int preco;
+    char desc[100 + 1];
+    float peso, preco;
 } REGISTRO;
 
 typedef struct
@@ -25,18 +33,16 @@ typedef struct
 
 /*struct do carrinho*/
 
-typedef struct aux
-{
+typedef struct nodo {
     REGISTRO reg;
-    struct aux* PROX;
-} ELEMENTO;
-
-typedef ELEMENTO* PONT;
+    struct nodo* PROX;
+} NODO, * PONT;
 
 typedef struct
 {
     PONT topo;
 } CARRINHO;
+
 
 // Declaração das funções
 int vertamanho(PILHA* p);
@@ -46,6 +52,11 @@ bool estaCheia(PILHA* p);
 bool inserirElemPilha(PILHA* p, REGISTRO reg);
 bool excluirElemPilha(PILHA* p, REGISTRO* reg);
 void reinicializarPilha(PILHA* p);
+void adicionarproduto(PILHA gondolas[]);
+void salvarPilhaEmArquivo(PILHA* p, const char* nomeArquivo);
+bool carregarPilhaDeArquivo(PILHA* p, const char* nomeArquivo);
+void exibirRegistros(reg* registros, int n);
+bool moveritemparacarrinho(PILHA* gondola, CARRINHO* carrinho);
 // Funções de inicialização
 void inicializarGondolas(PILHA gondolas[]);
 void inicializarPilha(PILHA* p);
